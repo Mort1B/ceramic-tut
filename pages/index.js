60d105f2776064a853e9379ef4b11dab823413f7
Morten Bergseng <bergseng.m@gmail.com>
2022-05-12 15:00:03 +0200

    done

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,57 @@ import { useEffect, useRef, useState } from 'react'
 import Web3Modal from "web3modal";
 import { useViewerConnection } from '@self.id/react'
 import { EthereumAuthProvider } from '@self.id/web'
+import { useViewerRecord } from "@self.id/react"
+
+
+function RecordSetter() {
+
+  const [name, setName] = useState("");
+  const record = useViewerRecord("basicProfile");
+  const updateRecordName = async (name) => {
+    await record.merge({
+      name: name,
+    })
+  }
+  return (
+    <div className={styles.content}>
+      <div className={styles.mt2}>
+        {record.content ? (
+          <div className={styles.flexCol}>
+            <span className={styles.subtitle}>
+              Hello {record.content.name}!
+            </span>
+
+            <span>
+              The above name was loaded from Ceramic Network. Try updating it
+              below.
+            </span>
+          </div>
+        ) : (
+          <span>
+            You do not have a profile record attached to your 3ID. Create a
+            basic profile by setting a name below.
+          </span>
+        )}
+      </div>
+
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className={styles.mt2}
+      />
+      <button onClick={() => updateRecordName(name)}>Update</button>
+    </div>
+  );
+}
 
 export default function Home() {
 
   const [connection, connect, disconnect] = useViewerConnection();
 
-  const Web3Modal = useRef();
+  const web3ModalRef = useRef();
 
   const getProvider = async () => {
     const provider = await web3ModalRef.current.connect();
@@ -37,9 +82,9 @@ export default function Home() {
         network: "rinkeby",
         providerOptions: {},
         disableInjectedProvider: false,
-      })
+      });
     }
-  }, [connection.status])
+  }, [connection.status]);
 
   return (
     <div className={styles.main}>
